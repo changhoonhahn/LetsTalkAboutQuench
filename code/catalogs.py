45a0459ad7508de653d9d300d51623e1a03f4a92
ChangHoon Hahn <chh327@nyu.edu>
2017-06-01 17:55:44 -0400

    catalog i/o implemented

--- code/catalogs.py
+++ code/catalogs.py
@@ -1,50 +1,93 @@
+import os 
 import numpy as np 
+import dill as pickle 
+
+# --- Local ---
+import util as UT
+
 
     
     
     
 class Catalog: 
-    ''' class object for different catalogs
-    '''
+    def __init__(self): 
+        ''' class object for reading in log(M*) and log(SFR)s of various catalogs
+        '''
+        self.catalog_dict = {
+                'santacruz1': 'sc_sam_sfr_mstar_correctedweights.txt', 
+                'santacruz2': 'sc_sam_sfr_mstar_correctedweights.txt', 
+                'tinkergroup': 'tinker_SDSS_centrals_M9.7.dat',
+                'illustris1': 'Illustris1_SFR_M_values.csv', 
+                'illustris2': 'Illustris1_SFR_M_values.csv', 
+                'nsa_dickey': 'dickey_NSA_iso_lowmass_gals.txt', 
+                'mufasa': 'Mufasa_m50n512_z0.cat'
+                }
 
-    def __init__(self, name): 
+    def Read(self, name): 
+        ''' Here we deal with the disparate outputs of the different catalogs and output
+        log(M*), log(SFR), weight 
 
+        name : (string) 
+            catalog name e.g. 'santacruz1'
+        '''
+        f_name = ''.join([UT.dat_dir(), self._File(name)]) 
+        if name == 'santacruz1': # santa cruz log(SFR) [10^5 yr]
+            sc_logM, sc_logSFR1, sc_logw = np.loadtxt(f_name, unpack=True, skiprows=1, usecols=[0,1,3])  
+            logM = sc_logM 
+            logSFR = sc_logSFR1
+            w = 10**sc_logw 
+        elif name == 'santacruz2': # santa cruz log(SFR) [10^8 yr]
+            sc_logM, sc_logSFR2, sc_logw = np.loadtxt(f_name, unpack=True, skiprows=1, usecols=[0,2,3])  
+            logM = sc_logM 
+            logSFR = sc_logSFR2
+            w = 10**sc_logw 
+        elif name == 'tinkergroup': # Tinker group catalog 
+            tink_Mstar, tink_logSSFR = np.loadtxt(f_name, unpack=True, skiprows=2, usecols=[0, 7])
+            logM = np.log10(tink_Mstar)
+            logSFR = tink_logSSFR + logM
+            w = np.ones(len(logM))
+        elif name == 'illustris1': # Illustris SFR timescale = 2 x 10^7 yr
+            ill_logMstar, ill_ssfr1 = np.loadtxt(f_name, unpack=True, skiprows=1, delimiter=',', usecols=[0,1])
+            logM = ill_logMstar
+            logSFR = np.log10(ill_ssfr1) + ill_logMstar
+            w = np.ones(len(logM))
+        elif name == 'illustris2': # Illustris SFR timescale = 10^9 yr
+            ill_logMstar, ill_ssfr2 = np.loadtxt(f_name, unpack=True, skiprows=1, delimiter=',', usecols=[0,2])
+            logM = ill_logMstar
+            logSFR = np.log10(ill_ssfr2) + ill_logMstar
+            w = np.ones(len(logM))
+        elif name == 'nsa_dickey': 
+            dic_logM, dic_logSFR =  np.loadtxt(f_name, unpack=True, skiprows=1, usecols=[1,5]) 
+            logM = dic_logM            
+            logSFR = dic_logSFR 
+            w = np.ones(len(logM))
+        elif name == 'mufasa':
+            muf_M, muf_SSFR =  np.loadtxt(f_name, unpack=True, skiprows=1, usecols=[0, 1]) 
+            logM = np.log10(muf_M)
+            logSFR = np.log10(muf_SSFR) + logM
+            w = np.ones(len(logM))
+        else: 
+            raise ValueError('')
+        # deal with sfr = 0 
+        sfr_zero = np.where(logSFR == -np.inf)
+        print len(sfr_zero[0]), ' of ', len(logM), ' galaxies have 0 SFR; logSFR of these galaxies will be -999.'
+        logSFR[sfr_zero] = -999.
 
-    def
+        return [logM, logSFR, w]
+
+    def _File(self, name): 
+        ''' catalog file names
+        '''
+        if name not in self.catalog_dict.keys(): 
+            raise ValueError('catalog not yet included')
+        else: 
+            return self.catalog_dict[name]
+
+if __name__=='__main__': 
+    Cat = Catalog()
     
-    
-    # Read in various data sets
-    logSFRs, logMstars, data_labels = [], [], [] 
-    ## santa cruz
-    sc_logM, sc_logSFR1, sc_logSFR2, sc_w = np.loadtxt("sc_sam_sfr_mstar_correctedweights.txt", unpack=True, skiprows=1) # logM*, logSFR (10^5yr), logSFR(10^8yr)
-    logSFRs.append(sc_logSFR1)
-    logMstars.append(sc_logM)
-    data_labels.append('SantaCruz')
-    ## Tinker group catalog 
-    tink_Mstar, tink_logSSFR = np.loadtxt('tinker_SDSS_centrals_M9.7.dat', unpack=True, skiprows=2, usecols=[0, 7])
-    tink_logM = np.log10(tink_Mstar)
-    tink_logSFR = tink_logSSFR + tink_logM
-    logSFRs.append(tink_logSFR)
-    logMstars.append(tink_logM)
-    data_labels.append('Tinker')
-    ## illustris
-    ill_logMstar, ill_ssfr1, ill_ssfr2 = np.loadtxt('Illustris1_SFR_M_values.csv', unpack=True, skiprows=1, delimiter=',')
-    ill_logSFR1 = np.log10(ill_ssfr1) + ill_logMstar
-    ill_logSFR2 = np.log10(ill_ssfr2) + ill_logMstar
-    logSFRs.append(ill_logSFR1)
-    logMstars.append(ill_logMstar)
-    data_labels.append('Illustris')
-    # NSA 
-    dic_logM, dic_logSFR =  np.loadtxt('dickey_NSA_iso_lowmass_gals.txt', unpack=True, skiprows=1, usecols=[1,5]) 
-    logSFRs.append(dic_logSFR)
-    logMstars.append(dic_logM)
-    data_labels.append('NSA')
-    # MUFASA
-    muf_M, muf_SSFR =  np.loadtxt('Mufasa_m50n512_z0.cat', unpack=True, skiprows=1, usecols=[0, 1]) 
-    muf_logM = np.log10(muf_M)
-    muf_logSSFR = np.log10(muf_SSFR)
-    muf_logSFR = muf_logSSFR + muf_logM
-    logSFRs.append(muf_logSFR)
-    logMstars.append(muf_logM)
-    data_labels.append('MUFASA')
-
+    for cata in ['santacruz1', 'santacruz2', 'tinkergroup', 'illustris1', 'illustris2', 'nsa_dickey', 'mufasa']: 
+        logM, logSFR, w = Cat.Read(cata)
+        print cata
+        print logM[:10], logSFR[:10], w[:10]
+        print '------------------------------------------------------------'
