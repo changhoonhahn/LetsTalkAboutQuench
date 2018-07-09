56acc86c504b0d74179376196da57effb32be498
ChangHoon Hahn <chh327@nyu.edu>
2018-07-09 17:58:22 -0400

    SFMS fits for high z simulations

--- doc/paper4.py
+++ doc/paper4.py
@@ -5,11 +5,7 @@ IQ paper 4 --- High z galaxies
 
 '''
 import numpy as np 
-import scipy as sp
 import corner as DFM 
-from scipy import linalg
-from scipy.stats import multivariate_normal as MNorm
-from sklearn.mixture import GaussianMixture as GMix
 # -- letstalkaboutquench --
 from letstalkaboutquench import util as UT
 from letstalkaboutquench import catalogs as Cats
@@ -32,23 +28,59 @@ mpl.rcParams['ytick.major.width'] = 1.5
 mpl.rcParams['legend.frameon'] = False
 
 
+def readHighz(name, i_z, keepzeros=False): 
+    ''' read in CANDELS, Illustris, or EAGLE M* and SFR data in the 
+    iz redshift bin 
+    '''
+    dat_dir = ''.join([UT.dat_dir(), 'highz/'])
+
+    name = name.lower() 
+    if name == 'candels': 
+        f_data = ''.join([dat_dir, 'CANDELS/CANDELS_z', str(i_z), '.txt']) 
+        _, ms, sfr = np.loadtxt(f_data, skiprows=2, unpack=True) # z, M*, SFR
+        logms = np.log10(ms) 
+        logsfr = np.log10(sfr) 
+        notzero = (sfr != 0.)
+    elif 'illustris' in name: 
+        f_data = ''.join([dat_dir, 'Illustris/Illustris_z', str(i_z), '.txt']) 
+        if name == 'illustris_10myr': 
+            ms, sfr, _ = np.loadtxt(f_data, skiprows=2, unpack=True) # M*, SFR 10Myr, SFR 1Gyr 
+        elif name == 'illustris_1gyr': 
+            ms, _, sfr = np.loadtxt(f_data, skiprows=2, unpack=True) # M*, SFR 10Myr, SFR 1Gyr 
+        logms = np.log10(ms) 
+        logsfr = np.log10(sfr) 
+        notzero = (sfr != 0.)
+    elif name == 'eagle': 
+        f_data = ''.join([dat_dir, 'EAGLE/EAGLE_z', str(i_z), '.txt']) 
+        ms, sfr = np.loadtxt(f_data, skiprows=2, unpack=True) # M*, SFR instantaneous
+        logms = ms
+        logsfr = sfr 
+        notzero = np.isfinite(sfr)
+    else: 
+        raise NotImplementedError
+    if not keepzeros: 
+        return logms[notzero], logsfr[notzero]
+    else: 
+        return logms, logsfr, notzero
+
+
 def candels(): 
     ''' SFR -- M* relation of CANDLES galaxies in the 6 redshift bins
     '''
     zlo = [0.5, 1., 1.4, 1.8, 2.2, 2.6]
     zhi = [1., 1.4, 1.8, 2.2, 2.6, 3.0]
 
-    datas = [] 
-    for i in range(1,7): 
-        f_candels = ''.join([UT.dat_dir(), 'highz/CANDELS/CANDELS_z', str(i), '.txt']) 
-        data = np.loadtxt(f_candels, skiprows=2, unpack=True) # z, M*, SFR
-        datas.append(data)
+    logms, logsfrs = [], [] 
+    for i in range(1,len(zlo)+1): 
+        logm, logsfr = readHighz('candels', i, keepzeros=False)
+        logms.append(logm)
+        logsfrs.append(logsfr)
     
     fig = plt.figure(figsize=(12,8))
     bkgd = fig.add_subplot(111, frameon=False)
-    for i_z, data in enumerate(datas): 
+    for i_z in range(len(zlo)): 
         sub = fig.add_subplot(2,3,i_z+1) 
-        DFM.hist2d(np.log10(data[1]), np.log10(data[2]), color='C0', 
+        DFM.hist2d(logms[i_z], logsfrs[i_z], color='C0', 
                 levels=[0.68, 0.95], range=[[7.8, 12.], [-4., 4.]], 
                 plot_datapoints=True, fill_contours=False, plot_density=True, 
                 ax=sub) 
@@ -67,7 +99,7 @@ def candels():
     fig.savefig(fig_name, bbox_inches='tight')
    
 
-def candels_sfms():
+def highz_sfms(name):
     ''' SFMS fits to the SFR -- M* relation of CANDLES galaxies in 
     the 6 redshift bins
     '''
@@ -75,14 +107,14 @@ def candels_sfms():
     zhi = [1., 1.4, 1.8, 2.2, 2.6, 3.0]
 
     fSFMS = fstarforms()
-    datas = [] 
+    logms, logsfrs = [], [] 
     sfms_fits = [] 
-    for i in range(1,7): 
-        f_candels = ''.join([UT.dat_dir(), 'highz/CANDELS/CANDELS_z', str(i), '.txt']) 
-        data = np.loadtxt(f_candels, skiprows=2, unpack=True) # z, M*, SFR
-        datas.append(data)
+    for i in range(1,len(zlo)+1): 
+        logm, logsfr = readHighz(name, i, keepzeros=False)
+        logms.append(logm)
+        logsfrs.append(logsfr)
         # fit the SFMSes
-        sfms_fit = fSFMS.fit(np.log10(data[1]), np.log10(data[2]), 
+        sfms_fit = fSFMS.fit(logm, logsfr, 
                 method='gaussmix',      # Gaussian Mixture Model fitting 
                 fit_range=[8.5, 12.0],  # stellar mass range
                 dlogm = 0.4,            # stellar mass bins of 0.4 dex
@@ -94,9 +126,9 @@ def candels_sfms():
     # SFMS overplotted ontop of SFR--M* relation 
     fig = plt.figure(figsize=(12,8))
     bkgd = fig.add_subplot(111, frameon=False)
-    for i_z, data in enumerate(datas): 
+    for i_z in range(len(zlo)): 
         sub = fig.add_subplot(2,3,i_z+1) 
-        DFM.hist2d(np.log10(data[1]), np.log10(data[2]), color='C0', 
+        DFM.hist2d(logms[i_z], logsfrs[i_z], color='C0', 
                 levels=[0.68, 0.95], range=[[7.8, 12.], [-4., 4.]], 
                 plot_datapoints=True, fill_contours=False, plot_density=True, 
                 ax=sub) 
@@ -107,14 +139,14 @@ def candels_sfms():
         sub.text(0.95, 0.05, '$'+str(zlo[i_z])+'< z <'+str(zhi[i_z])+'$', 
                 ha='right', va='bottom', transform=sub.transAxes, fontsize=20)
         if i_z == 0: 
-            sub.text(0.05, 0.95, 'CANDELS', 
-                    ha='left', va='top', transform=sub.transAxes, fontsize=25)
+            sub.text(0.05, 0.95, ' '.join(name.upper().split('_')),
+                    ha='left', va='top', transform=sub.transAxes, fontsize=20)
 
     bkgd.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
     bkgd.set_xlabel(r'log ( $M_* \;\;[M_\odot]$ )', labelpad=15, fontsize=25) 
     bkgd.set_ylabel(r'log ( SFR $[M_\odot \, yr^{-1}]$ )', labelpad=15, fontsize=25) 
     fig.subplots_adjust(wspace=0.2, hspace=0.15)
-    fig_name = ''.join([UT.doc_dir(), 'highz/figs/candels_sfms.pdf'])
+    fig_name = ''.join([UT.doc_dir(), 'highz/figs/', name.lower(), '_sfms.pdf'])
     fig.savefig(fig_name, bbox_inches='tight')
     
     # SFMS fit redshift evolution 
@@ -124,14 +156,15 @@ def candels_sfms():
         sub.fill_between(sfms_fit[0], sfms_fit[1]-sfms_fit[2], sfms_fit[1]+sfms_fit[2], 
                 color='C'+str(i_z), linewidth=0.5, alpha=0.75, 
                 label='$'+str(zlo[i_z])+'< z <'+str(zhi[i_z])+'$') 
+    sub.text(0.95, 0.05, ' '.join(name.upper().split('_')),
+            ha='right', va='bottom', transform=sub.transAxes, fontsize=20)
     sub.legend(loc='upper left', prop={'size':15}) 
     sub.set_xlim([8.5, 12.]) 
     sub.set_ylim([-0.5, 4.]) 
-    fig_name = ''.join([UT.doc_dir(), 'highz/figs/candels_sfmsz.pdf'])
+    fig_name = ''.join([UT.doc_dir(), 'highz/figs/', name.lower(), '_sfmsz.pdf'])
     fig.savefig(fig_name, bbox_inches='tight')
     return None 
 
 
 if __name__=="__main__": 
-    #candels() 
-    candels_sfms()
+    highz_sfms('eagle')
